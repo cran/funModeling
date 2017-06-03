8df5813c0fb517e64f9592dea3c746695c90ea3e
Pablo Casas <pcasas.biz@gmail.com>
2017-06-03 22:31:35 +0000

    version 1.6.3

--- R/cross_plot.R
+++ R/cross_plot.R
@@ -1,4 +1,4 @@
-utils::globalVariables(names=c("fum","element_blank","value","ratio","aes","variable","geom_bar","geom_text","position","guides","labs","theme","element_text","scale_y_continuous","position_dodge","ylim","guide_legend","scale_fill_discrete", "aes_string", "geom_boxplot","stat_summary", "theme_bw", "freq", "geom_vline", "geom_density", "margin", "scale_colour_continuous",'Var1','label','coord_flip','ylab','xlab','geom_label','unit','Population','Gain','Score.Point','geom_line','geom_point','xlim','geom_segment','Lift', 'Freq', 'sum_pos', 'likelih','.','one_of','grp_mean', 'mean_target',"'colorRampPalette","head","tail","rbeta"), package = "funModeling", add = F)
+utils::globalVariables(names=c("fum","element_blank","value","ratio","aes","variable","geom_bar","geom_text","position","guides","labs","theme","element_text","scale_y_continuous","position_dodge","ylim","guide_legend","scale_fill_discrete", "aes_string", "geom_boxplot","stat_summary", "theme_bw", "freq", "geom_vline", "geom_density", "margin", "scale_colour_continuous",'Var1','label','coord_flip','ylab','xlab','geom_label','unit','Population','Gain','Score.Point','geom_line','geom_point','xlim','geom_segment','Lift', 'Freq', 'sum_pos', 'likelih','.','one_of','grp_mean', 'mean_target',"'colorRampPalette","head","tail","rbeta","p_10","p_90","sd" ,"std_dev","variation_coef","iqr","type"), package = "funModeling", add = F)
 
 
 #' @importFrom grDevices dev.off jpeg rainbow
@@ -17,8 +17,9 @@ utils::globalVariables(names=c("fum","element_blank","value","ratio","aes","vari
 #' @importFrom stats cor quantile
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom grDevices colorRampPalette
-#' @importFrom stats kmeans rbeta hclust cutree dist
+#' @importFrom stats kmeans rbeta hclust cutree dist IQR na.omit sd
 #' @importFrom utils head tail
+#' @importFrom moments skewness kurtosis
 
 #' @title Cross-plotting input variable vs. target variable
 #' @description The cross_plot shows how the input variable is correlated with the target variable, getting the likelihood rates for each input's bin/bucket .
@@ -239,17 +240,14 @@ cross_plot_logic<-function(data, str_input, str_target, path_out, auto_binning, 
 #' @export
 equal_freq <- function(var, n_bins)
 {
-  n_bins_orig=n_bins
-
-  ## Call the cut2 from Hmisc
-  res=cut2(var, g = n_bins)
-
-  n_bins_final=length(unique(res))
-
-  if(n_bins_final != n_bins)
-    warning(sprintf("It's not possible to calculate with n_bins=%s, setting n_bins in: %s.", n_bins, n_bins_final))
-
-  return(res)
+	n_bins_orig = n_bins
+	res = cut2(var, g = n_bins)
+	uq=unique(res)
+	n_bins_final = length(uq)
+	if (n_bins_final != n_bins & sum(is.na(uq))==0)
+		warning(sprintf("It's not possible to calculate with n_bins=%s, setting n_bins in: %s.",
+										n_bins, n_bins_final))
+	return(res)
 }
 
 
